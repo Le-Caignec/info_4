9ecf1b78e730a74144e4ad252aeec2c2955c23fa
Robin <robinlecaignec@yahoo.fr>
2020-11-01 16:11:48 +0100

    new

---
+++ fr.The_Xtrem_devs.PIF/src/main/java/fr/The_Xtrem_devs/PIF/App.java
@@ -0,0 +1,13 @@
+package fr.The_Xtrem_devs.PIF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
